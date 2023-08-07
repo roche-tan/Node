0cf2d738a2407a60b80890cb18de5bf53c8af889
roche-tan <rochelyn.rallestan@gmail.com>
2023-08-07 19:45:22 +0200

    how to read files

--- Working with modules/inedx.js
+++ Working with modules/inedx.js
@@ -10,10 +10,30 @@ console.log(os.release());
 console.log('free mem: ', os.freemem(), ' bytes');
 console.log('total mem: ', os.totalmem(), ' bytes');
 
-// how to create a new file. Has 3 params: name, content and callback. Name. where to create it - in this case ./ as it will be in the same folder. Callback is a function called when the process is finished to know if everythig was made ok
+//File system (fs) how to create a new file. Has 3 params: name, content and callback. Name. where to create it - in this case ./ as it will be in the same folder. Callback is a function called when the process is finished to know if everythig was made ok
 fs.writeFile('./text.txt', 'line one', function(err){
   if(err){
     console.log(err);
   }
   console.log('File created');
+});
+
+/*const users = query('SELECT * FROM Users'); 
+ Async code
+query('Select * FROM users', function(err, users){
+  if(err){
+    console.log(err);
+  }
+  if(users){
+    console.log(users);
+  }
+})*/
+
+//read file
+fs.readFile('./text.txt', function(err, data){
+  if(err){
+    console.log(err);
+  }
+  //console.log(data); shows buffer
+  console.log(data.toString()); //shows in string
 });
